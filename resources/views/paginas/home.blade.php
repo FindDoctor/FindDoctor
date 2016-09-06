c92c4f95f479b431933175761afd5059aba5b218
bertZ <lucasbertz@hotmail.com>
2016-09-06 13:42:20 +0000

    Adicionado exemplo de pagina

--- resources/views/paginas/home.blade.php
+++ resources/views/paginas/home.blade.php
@@ -2,8 +2,27 @@
 
 @section('titulo', 'Home')
 
+@section('css')
+	@parent
+
+	<style>
+		body {
+			padding-top: 50px;
+			padding-bottom: 20px;
+		}
+	</style>
+@endsection
+
 @section('conteudo')
 	<h1>Hello, world!</h1>
 	<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
 	<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+@endsection
+
+@section('js')
+	@parent
+	
+	<script>
+		alert("teste")
+	</script>
 @endsection
