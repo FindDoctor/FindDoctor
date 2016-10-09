bb6c7bda1af3c4fc3ce8e3cf7d93c730b00cb4f1
Gruutak <lucasbertz@hotmail.com>
2016-10-09 14:48:22 +0000

    Mudanças no js - Limpeza de codigo (InitMap movida para o arquivo app.js) - Mapa tem marcador centralizado na localização do usuário caso o usuário permita o uso da geolocalização

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,1 +1,35 @@
 require('./bootstrap');
+
+function initMap(latitude, longitude) {
+	var uluru = {lat: latitude, lng: longitude};
+
+	console.log(uluru);
+
+	var mapOptions = {
+		center: uluru,
+		zoom: 14,
+	}
+
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	var markerOptions = {
+		position: uluru,
+		map: map,
+		title: "Sua localização"
+	}
+
+	var marker = new google.maps.Marker(markerOptions);
+}
+
+
+$(document).ready(function () {
+	if (navigator.geolocation)
+		navigator.geolocation.getCurrentPosition(function (position) {
+			initMap(position.coords.latitude, position.coords.longitude);
+		},
+		function () {
+			initMap(-15.7217509, -48.0082761);
+		});
+	else
+		initMap(-48.0082761,-15.7217509);
+});
--- resources/views/pages/inicio.blade.php
+++ resources/views/pages/inicio.blade.php
@@ -108,23 +108,7 @@
 @section('js')
 		@parent
 
-		<script>
-		function initMap() {
-			var uluru = {lat: -25.363, lng: 131.044};
-			var map = new google.maps.Map(document.getElementById('map'), {
-			  zoom: 4,
-			  center: uluru
-			});
-			var marker = new google.maps.Marker({
-			  position: uluru,
-			  map: map
-			});
-		}
-		</script>
-
-        <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAguzcFY7o2fh08MZKHlIisrTvxrHBE1Z4&callback=initMap">
-        </script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAguzcFY7o2fh08MZKHlIisrTvxrHBE1Z4"></script>
 @endsection
     </body>
 </html>
