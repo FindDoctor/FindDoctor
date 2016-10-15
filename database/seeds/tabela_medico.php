fed5bf979990c5a978377c0552c2c3a65fabe001
vinixxyama <vini_yamamoto@hotmail.com>
2016-10-15 23:34:26 +0000

    geracao de dados automatico para paciente e medico

---
+++ database/seeds/tabela_medico.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+use Faker\Factory as Faker;
+
+class tabela_medico extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	$faker = Faker::create();
+    	$faker->addProvider(new \Faker\Provider\pt_BR\PhoneNumber($faker));
+    	foreach(range(1, 5) as $index){
+        	DB::table('medicos')->insert([
+        		'crm' => $faker->numberBetween($min = 1000000, $max = 9999999),
+        		'nome' => $faker->name,
+        		'email' => $faker->email,
+	            'password' => $faker->password,
+	            'endereco' => $faker->address,
+	            'telefone' => $faker->landlineNumber,
+        	]);
+    	}
+    }
+}
