edab0fd0ad09de04f868da6e6d01dbb358f2d478
bertZ <lucasbertz@hotmail.com>
2016-11-04 13:29:55 -0200

    Limpeza no codigo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,35 +40,38 @@ class AppServiceProvider extends ServiceProvider
             $cpf = preg_replace('[^0-9]', '', $value);
             $cpf = str_pad($cpf, 11, '0', STR_PAD_LEFT);
 
+			$cpfsProibidos = [
+				'00000000000',
+				'11111111111',
+				'22222222222',
+				'33333333333',
+				'44444444444',
+				'55555555555',
+				'66666666666',
+				'77777777777',
+				'88888888888',
+				'99999999999'
+			];
+
 			// Verifica se o numero de digitos informados é igual a 11
             if (strlen($cpf) != 11) return false;
 
 			// Verifica se nenhuma das sequências invalidas abaixo
 			// foi digitada. Caso afirmativo, retorna falso
-            else if ($cpf == '00000000000' ||
-                     $cpf == '11111111111' ||
-                     $cpf == '22222222222' ||
-                     $cpf == '33333333333' ||
-                     $cpf == '44444444444' ||
-                     $cpf == '55555555555' ||
-                     $cpf == '66666666666' ||
-                     $cpf == '77777777777' ||
-                     $cpf == '88888888888' ||
-                     $cpf == '99999999999')
-                        return false;
+            else if (in_array($cpf, $cpfsProibidos))
+                return false;
 
 			// Calcula os digitos verificadores para verificar se o
 			// CPF é válido
 			else {
 			    for ($t = 9; $t < 11; $t++) {
-
-			        for ($d = 0, $c = 0; $c < $t; $c++) {
+			        for ($d = 0, $c = 0; $c < $t; $c++)
 			            $d += $cpf{$c} * (($t + 1) - $c);
-			        }
+
 			        $d = ((10 * $d) % 11) % 10;
-			        if ($cpf{$c} != $d) {
-			            return false;
-			        }
+
+			        if ($cpf{$c} != $d)
+						return false;
 			    }
 
 			    return true;
