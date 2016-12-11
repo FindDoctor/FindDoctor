b4463b672b94c692fddb9dec9b109ae1b84c69ce
Gruutak <lucasbertz@hotmail.com>
2016-12-11 13:01:05 +0000

    Adicionada mascara de cpf no registro de rede social

--- resources/views/auth/socialRegister.blade.php
+++ resources/views/auth/socialRegister.blade.php
@@ -29,7 +29,7 @@
                             <label for="cpf" class="col-md-4 control-label">CPF</label>
 
                             <div class="col-md-6">
-                                <input id="cpf" type="text" class="form-control" name="cpf" value="{{ old('cpf') }}" required autofocus>
+                                <input id="cpf_input" type="text" class="form-control" name="cpf" value="{{ old('cpf') }}" required autofocus>
 
                                 @if ($errors->has('cpf'))
                                     <span class="help-block">
