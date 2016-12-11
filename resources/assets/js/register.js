5749d786476fd5f2472d56e8e557327e934d9b15
Gruutak <lucasbertz@hotmail.com>
2016-12-11 21:43:10 +0000

    Finalizadas mascaras no cadastro

--- resources/assets/js/register.js
+++ resources/assets/js/register.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-   $('#cpf_input').mask('000.000.000-00', {reverse: true});
-   $('#cep').mask('00000-000');
-});
+
 
 $('#tipoPaciente').click(function () {
 	$('#tipoCadastro').hide();
@@ -87,4 +84,20 @@ if($("#consultorios-list").children().length >= 5){
 $('#btn-add').click(function(){
 	$('#li_novo_consultorio').show();
 	$(this).hide();
+});
+
+$(document).ready(function(){
+	$('#cpf_input').mask('000.000.000-00', {reverse: true});
+	$('#cep').mask('00000-000');
+
+	var SPMaskBehavior = function (val) {
+		return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+	},
+	spOptions = {
+		onKeyPress: function(val, e, field, options) {
+			field.mask(SPMaskBehavior.apply({}, arguments), options);
+		}
+	};
+
+	$('#telefone').mask(SPMaskBehavior, spOptions);
 });
